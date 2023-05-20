84736d3ae9cd29f4ab79b984107c92027067c879
MelihCelik00 <melihsafa.c@gmail.com>
2023-05-20 13:37:44 +0300

    set routes for components

--- backend/routes/api.route.js
+++ backend/routes/api.route.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
+const authLayer = require('../middlewares/authlayer');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'To-Do App' });
 });
+
+router.use('/auth', require('../components/auth.route'));
+router.use('/todo', require('../components/todo.route'));
 
 module.exports = router;
