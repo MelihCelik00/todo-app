ec0d00da801bc1cedbda17c1c4ca285e30213251
MelihCelik00 <melihsafa.c@gmail.com>
2023-05-20 17:43:11 +0300

    editted config.js

--- backend/config/config.js
+++ backend/config/config.js
@@ -1,53 +1,25 @@
-const {
-  POSTGRES_USER,
-  POSTGRES_PASSWORD,
-  POSTGRES_DB,
-  POSTGRES_SERVICE_NAME,
-  POSTGRES_PORT,
-  NODE_ENV
-} = process.env;
+require('dotenv').config();
 
-let options = {
-  username: POSTGRES_USER,
-  password: POSTGRES_PASSWORD,
-  database: POSTGRES_DB,
-  host: POSTGRES_SERVICE_NAME,
-  port: POSTGRES_PORT,
-  dialect: 'postgres',
-  seederStorage: 'sequelize',
-  logging: false,
-  // query: {
-  //     raw: true
-  // }
-};
-
-if (NODE_ENV !== 'local')
-  options.dialectOptions = {
-      ssl: true
-  };
-
-module.exports = options;
-
-// {
-//   "development": {
-//     "username": process.env.POSTGRES_USER,
-//     "password": process.env.POSTGRES_PASSWORD,
-//     "database": process.env.POSTGRES_DB,
-//     "host": process.env.POSTGRES_SERVICE_NAME,
-//     "dialect": "postgres"
-//   },
-//   "test": {
-//     "username": process.env.POSTGRES_USER,
-//     "password": process.env.POSTGRES_PASSWORD,
-//     "database": process.env.POSTGRES_DB,
-//     "host": process.env.POSTGRES_SERVICE_NAME,
-//       "dialect": "postgres"
-//   },
-//   "production": {
-//     "username": process.env.POSTGRES_USER,
-//     "password": process.env.POSTGRES_PASSWORD,
-//     "database": process.env.POSTGRES_DB,
-//     "host": process.env.POSTGRES_SERVICE_NAME,
-//       "dialect": "postgres"
-//   }
-// }
+module.exports = {
+    development: {
+        username: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        host: process.env.DB_HOST,
+        dialect: process.env.DB_DIALECT
+    },
+    test: {
+        username: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        host: process.env.DB_HOST,
+        dialect: process.env.DB_DIALECT
+    },
+    production: {
+        username: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        host: process.env.DB_HOST,
+        dialect: process.env.DB_DIALECT
+    }
+}; 
