d2235faeaaaa61a4671e2c549b199affd589ea46
MelihCelik00 <melihsafa.c@gmail.com>
2023-05-20 21:34:35 +0300

    swagger added to app.js

--- backend/app.js
+++ backend/app.js
@@ -8,15 +8,17 @@ const port = process.env.PORT || 8080
 
 require('dotenv').config();
 
-var indexRouter = require('./routes/api.route');
-var usersRouter = require('./routes/users');
+const docs = require('./routes/swagger.route');
 
 var app = express();
+app.use('/api/documentation', docs.serve, docs.setup);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+var apiRouter = require('./routes/api.route');
+var usersRouter = require('./routes/users');
 
 app.use(cors())
 app.use(logger('dev'));
@@ -25,8 +27,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/api', apiRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
