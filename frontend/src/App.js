f50ae9aa9b96567ba33343c97fff40561f8e4d9a
MelihCelik00 <melihsafa.c@gmail.com>
2023-05-21 15:18:06 +0300

    final updates

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,18 @@
+import { useEffect, useState } from "react";
 import ListHeader from "./components/ListHeader";
 import ListItem from "./components/ListItem"
-import { useEffect, useState } from "react";
+import Auth from "./components/Auth";
+import { useCookies } from 'react-cookie';
 
 const App = () => {
-  const userId = 1; // hardcoded for now
-  const [ tasks, setTasks] = useState(null);
+  const [cookies, setCookie, removeCookie] = useCookies(null);
+  const userId = cookies.UserId;
+  const authToken = cookies.AuthToken;
+  const [tasks, setTasks] = useState(null);
 
   const getData = async () => {
     try {
       const response = await fetch(`${process.env.REACT_APP_SERVERURL}/api/todo/${userId}`);
-      console.log(userId);
       const json = await response.json();
       setTasks(json);
     } catch (error) {
@@ -17,17 +20,23 @@ const App = () => {
     }
   }
 
-  useEffect(() => { getData() }, []);
-
-  console.log(tasks);
+  useEffect(() => { 
+    if (authToken)
+      getData();
+   }, []);
 
   // Sort by date
   const sortedTasks = tasks?.sort((a, b) => new Date(a.date) - new Date(b.date));
 
   return (
     <div className="app">
+      {!authToken && <Auth />}
+      {authToken &&
+      <>
       <ListHeader listName={'To-Do App'} getData={getData}/>
       {sortedTasks?.map( (task) => <ListItem key={task.id} task={task} getData={getData}/>)}
+      </>
+      }      
     </div>
   );
 }
